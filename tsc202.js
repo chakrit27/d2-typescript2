d5fe16c3ecc21791655c24d5ed90f57738ed9113
chakrit27 <chakrit.indy2001@gmail.com>
2021-11-10 14:03:38 +0700

    initial commit

---
+++ tsc202.js
@@ -0,0 +1,6 @@
+var contries = ['Thailand', 'Singapore', 'Malaysia', null, undefined];
+function display(id, name) {
+    console.log("Id = " + id + " Name = " + name);
+}
+display(101, "Rob Jhonson");
+console.log(contries);
---
+++ tsc202.ts
@@ -0,0 +1,7 @@
+let contries: string[] = ['Thailand', 'Singapore', 'Malaysia', null, undefined];
+function display(id:number, name:string){
+    console.log("Id = " + id + " Name = " + name);
+}
+
+display(101 , "Rob Jhonson");
+console.log(contries);
